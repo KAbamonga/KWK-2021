5f3cd5b3c8bba5aa43641dd5ea8ec11dd4b8e188
KAbamonga <87456353+KAbamonga@users.noreply.github.com>
2021-07-15 09:23:53 -0400

    Version 1

---
+++ MiniProject1/MiniProject1/MiniProject1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Kiara Abamonga on 7/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var fact1Title: UILabel!
+    @IBOutlet weak var fact2Title: UILabel!
+    @IBOutlet weak var fact3Title: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func userTapped(_ sender: UIButton) {
+        
+    }
+    
+}
+
